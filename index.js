fbf633db0ba694c52806baf3b9dad229ca6f4801
Shital8596 <101247710+Shital8596@users.noreply.github.com>
2022-09-24 12:43:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var id;
 
 // To reply for every comment --------------->
 
-function doSomething(d,f,e){
+function addReply(d,f,e){
          z = z+1;
 
         var textContainer = document.getElementById(d);
@@ -32,12 +32,22 @@ function doSomething(d,f,e){
             innerContainer.classList.add("addNewReply");
             let newTextBox = document.createElement("textarea");
             newTextBox.classList.add("replyText");
+
+            let btnDiv = document.createElement("div");
+            btnDiv.classList.add("btnDiv");
+
             let textButton = document.createElement("button");
+            let replyCancelButton = document.createElement("button");
+            replyCancelButton.innerText = 'Cancel';
+            replyCancelButton.classList.add("replyBtn");
+            replyCancelButton.classList.add("replyCancelBtn");
             textButton.innerText = "Reply";
             textButton.classList.add("replyBtn");
         
             innerContainer.appendChild(newTextBox);
-            innerContainer.appendChild(textButton);
+            btnDiv.appendChild(textButton);
+            btnDiv.appendChild(replyCancelButton);
+            innerContainer.appendChild(btnDiv);
             textContainer.append(innerContainer);
 
             // After clicking reply button --------->
@@ -62,7 +72,7 @@ function doSomething(d,f,e){
                         <div class="comment_container" id="containerId${id}">
                         <div class="comment_card" id="cardId${id}">
                         <div class="commentHeader">
-                            <div class="commentImg D">S</div>
+                            <div class="commentImg SC">S</div>
                                 <h3>You</h3>
                                 <span>Few seconds ago</span>
                         </div>
@@ -72,7 +82,7 @@ function doSomething(d,f,e){
                         <div class="comment_footer">
                             <div class="like btn" onclick="likes('likeSpanId${id}')">Like <span id="likeSpanId${id}">0</span></div>
                             <div class="dislike btn" onclick="disLikes('dislikeSpanId${id}')">Dislike <span id="dislikeSpanId${id}">0</span></div>
-                            <div class="reply btn" id="one" onclick="doSomething('containerId${id}','cardId${id}','replySpanId${id}')">Reply <span id="replySpanId${id}">0</span></div>
+                            <div class="reply btn" id="one" onclick="addReply('containerId${id}','cardId${id}','replySpanId${id}')">Reply <span id="replySpanId${id}">0</span></div>
                             <div class="delete btn" onclick="deleteComment('containerId${id}')">Delete</div>
                         </div>
                         </div>
@@ -80,14 +90,24 @@ function doSomething(d,f,e){
                    `
 
                  replyContainer.append(innerContainer);
+
+                // To increase reply count
+
+                let span = document.getElementById(e);
+                span.innerHTML = parseInt(span.innerHTML) + 1;
+                z = 0;
             });
 
-        // To increase reply count
+        // To cancel the reply 
 
-        let span = document.getElementById(e);
-        span.innerHTML = parseInt(span.innerHTML) + 1;
-        z = 0;
-}
+        let replyCancelBtn = document.querySelector('.replyCancelBtn');
+
+        replyCancelBtn.addEventListener('click',()=>{
+            innerContainer.remove();
+        })
+
+
+}       
 
 var numb2;
 var id2 = 4;
@@ -104,8 +124,8 @@ function deleteComment(e){
 //------------->For Adding New Comment
 
  sendButton.addEventListener('click',()=>{
-     
-     const CommentTextArea = document.getElementById('commentText');
+
+    const CommentTextArea = document.getElementById('commentText');
     id2 = id2 + 1;
     console.log(parseInt(id2));
     let innerContainer = document.createElement("div");
@@ -117,7 +137,7 @@ function deleteComment(e){
     innerContainer.innerHTML = `<div class="comment_container" id="container${id2}">
     <div class="comment_card" id="card${id2}">
     <div class="commentHeader">
-        <div class="commentImg D">S</div>
+        <div class="commentImg SC">S</div>
             <h3>You</h3>
             <span>Few seconds ago</span>
     </div>
@@ -127,15 +147,13 @@ function deleteComment(e){
     <div class="comment_footer">
     <div class="like btn" onclick="likes('likeSpan${id2}')">Like <span id="likeSpan${id2}">0</span></div>
     <div class="dislike btn" onclick="disLikes('dislikeSpan${id2}')">Dislike <span id="dislikeSpan${id2}">0</span></div>
-        <div class="reply btn" id="one" onclick="doSomething('container${id2}','card${id2}','replySpan${id2}')">Reply <span id="replySpan${id2}">0</span></div>
+        <div class="reply btn" id="one" onclick="addReply('container${id2}','card${id2}','replySpan${id2}')">Reply <span id="replySpan${id2}">0</span></div>
         <div class="delete btn" onclick="deleteComment('container${id2}')">Delete</div>
     </div>
 </div>
 </div>
 `
-
-    if(CommentTextArea.value === ''){
-        alert("Comment should not be empty!!");
+    if(CommentTextArea.value === ''){alert("Comment should not be empty!!")
     }else{
         container.append(innerContainer);
     }
